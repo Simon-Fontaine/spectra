e884a247e242637d686c9fe047c1f231c3bcaa04
Simon-Fontaine <74004726+Simon-Fontaine@users.noreply.github.com>
2025-02-01 22:59:40 +0100

    fix: KV_REST_API_URL name

--- lib/redis.ts
+++ lib/redis.ts
@@ -5,6 +5,6 @@ import { Redis } from "@upstash/redis";
  * A Redis client instance configured via Upstash and environment variables.
  */
 export const redis = new Redis({
-  url: APP_CONFIG_PRIVATE.KV_URL,
+  url: APP_CONFIG_PRIVATE.KV_REST_API_URL,
   token: APP_CONFIG_PRIVATE.KV_REST_API_TOKEN,
 });
