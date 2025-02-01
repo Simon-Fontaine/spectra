571b24c9593463cf96a5f4a361ef0eefa0a3f5a2
Simon-Fontaine <74004726+Simon-Fontaine@users.noreply.github.com>
2025-02-01 17:57:52 +0100

    fix: client side env access

--- lib/config.public.ts
+++ lib/config.public.ts
@@ -1,35 +1,12 @@
-"use client";
-
-import { z } from "zod";
-
-const publicEnvSchema = z.object({
-  NEXT_PUBLIC_APP_NAME: z.string(),
-  NEXT_PUBLIC_APP_DESCRIPTION: z.string(),
-  NEXT_PUBLIC_APP_DOMAIN: z.string(),
-  NEXT_PUBLIC_APP_EMAIL: z.string().email(),
-  NEXT_PUBLIC_APP_HELP_EMAIL: z.string().email(),
-  NEXT_PUBLIC_VERCEL_URL: z.string().url().optional(),
-});
-
-const parsedPublicEnv = publicEnvSchema.safeParse(process.env);
-if (!parsedPublicEnv.success) {
-  console.error(
-    "❌ Invalid public environment variables",
-    parsedPublicEnv.error.format()
-  );
-  throw new Error("Invalid public environment variables");
-}
-
-const publicEnv = parsedPublicEnv.data;
-
 export const APP_CONFIG_PUBLIC = {
-  APP_NAME: publicEnv.NEXT_PUBLIC_APP_NAME,
-  APP_DESCRIPTION: publicEnv.NEXT_PUBLIC_APP_DESCRIPTION,
-  APP_DOMAIN: publicEnv.NEXT_PUBLIC_APP_DOMAIN,
-  APP_EMAIL: publicEnv.NEXT_PUBLIC_APP_EMAIL,
-  APP_HELP_EMAIL: publicEnv.NEXT_PUBLIC_APP_HELP_EMAIL,
-  APP_URL: publicEnv.NEXT_PUBLIC_VERCEL_URL || "http://localhost:3000",
+  APP_NAME: "Spectra",
+  APP_DESCRIPTION:
+    "Spectra is a 4.5K Overwatch 2 e-sport team that competes in diverse tournaments and leagues.",
+  APP_DOMAIN: "owspectra.com",
+  APP_EMAIL: "noreply@owspectra.com",
+  APP_HELP_EMAIL: "help@owspectra.com",
+  APP_URL: process.env.NEXT_PUBLIC_VERCEL_URL || "http://localhost:3000",
   APP_OG_IMAGE: `${
-    publicEnv.NEXT_PUBLIC_VERCEL_URL || "http://localhost:3000"
+    process.env.NEXT_PUBLIC_VERCEL_URL || "http://localhost:3000"
   }/images/og.png`,
 };
